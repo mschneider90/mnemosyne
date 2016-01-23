9ea143096e04fd3262cd5937a192412b10d2026a
Shane Loretz <Shane.Loretz@gmail.com>
2016-01-23 14:22:53 -0800

    Moved projectile offset to look more like it's comming out of the weapon

--- Source/MyProject/MyProjectCharacter.cpp
+++ Source/MyProject/MyProjectCharacter.cpp
@@ -44,7 +44,7 @@ AMyProjectCharacter::AMyProjectCharacter()
 
 
 	// Default offset from the character location for projectiles to spawn
-	GunOffset = FVector(100.0f, 30.0f, 10.0f);
+	GunOffset = FVector(100.0f, 20.0f, 45.0f);
 
 	// Note: The ProjectileClass and the skeletal mesh/anim blueprints for Mesh1P are set in the
 	// derived blueprint asset named MyCharacter (to avoid direct content references in C++)
